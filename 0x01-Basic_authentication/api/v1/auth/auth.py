edc07620e02d154cba54f91d6ba42b21ee6f5711
root <root@PC1.localdomain>
2024-09-04 13:08:37 +0100

    3.auth.py fix

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,27 +1,31 @@
 import request from flask
 from typing import list, TypeVar
 
+
 class Auth:
+
     """
     Auth class template for managing authentication
+
     """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        
+
         """
         Implementation of require_
         auth
-        """
 
+        """
         return False
 
     def authorization_header(self, request=None) -> str:
+
         """
         Implementation of
         authorization header
+
         """
 
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-
