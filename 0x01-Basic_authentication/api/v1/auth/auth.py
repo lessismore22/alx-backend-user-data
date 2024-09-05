12ba775b78345e59c8171f660aaa6ab6d5c456ca
root <root@PC1.localdomain>
2024-09-05 15:14:38 +0100

    ath fix

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -16,7 +16,20 @@ class Auth:
         auth
 
         """
-        return False
+
+        if path is None:
+            return True
+
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+         # Ensure path ends with a slash for comparison
+        path_with_slash = path if path.endswith('/') else path + '/'
+
+        # Check if the path matches any of the excluded paths
+        for excluded_path in excluded_paths:
+            if excluded_path == path_with_slash:
+                return False
+
 
     def authorization_header(self, request=None) -> str:
 
@@ -25,7 +38,17 @@ class Auth:
         authorization header
 
         """
+        if request is None:
+            return None
+        
+        if request.headers.get("Authorization", None)
 
-        return None
 
     def current_user(self, request=None) -> TypeVar('User'):
+
+        """
+        Retrieves the current user
+        from the flask object
+        """
+
+        return None
